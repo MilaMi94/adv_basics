9aa8b168dc17ad43404d1104dd54faa9d2b6ecca
MilaMi94 <mila.bogdanovic94@gmail.com>
2024-01-30 15:13:56 +0100

    questions_screen added

---
+++ lib/questions_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class QuestionsScreeen extends StatefulWidget {
+  const QuestionsScreeen({super.key});
+  @override
+  State<QuestionsScreeen> createState() {
+    return _QuestionsScreeenState();
+  }
+}
+
+class _QuestionsScreeenState extends State<QuestionsScreeen> {
+  @override
+  Widget build(context) {
+    return const Text('QuestionsScreen');
+  }
+}
